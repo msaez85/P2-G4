7f724603fdc5fbd6fd8ff9942263058ae0a2aa47
priscilaGarciaMoya <priscilagarciamoya@alu.frt.utn.edu.ar>
2023-05-25 18:21:08 -0300

    se agrega validaciones del formulario de contacto en html em js y funcionalidad de enviar el formulario al correo

--- js/contacto.js
+++ js/contacto.js
@@ -1,12 +1,42 @@
-
-const formEnviarComentario = document.getElementById('formEnviarComentario');
+import {validarFormulario} from'../helpers/help.js'; 
+const formEnviarComentario = document.getElementById('formEnviarComentario'),
+  btnEnviarMensaje = document.getElementById('enviarComentario');
 
 formEnviarComentario.addEventListener("submit", validarFormularioAgregarComic);
 function validarFormularioAgregarComic(event) {
-    if (!formEnviarComentario.checkValidity()) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+  event.preventDefault();
+  if (!formEnviarComentario.checkValidity()) {
+    event.stopPropagation();
     formEnviarComentario.classList.add('was-validated');
-    
+  } else {
+    const respuesta = validarInformacionFormulario(); 
+    if (respuesta !== null) {
+      formEnviarComentario.reset();
+      return alert(respuesta); 
+    }
+  
+    btnEnviarMensaje.innerText = 'Enviando...';
+    const serviceID = 'default_service';
+    const templateID = 'template_pcxppz7';
+  
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btnEnviarMensaje.innerText = 'Enviar Comentario';
+        alert('Mensaje Enviado!'); 
+        formEnviarComentario.reset();
+      }, (err) => {
+        btnEnviarMensaje.innerText = 'Enviar Comentario';
+        alert(`Mensaje No Enviado!. Error: ${JSON.stringify(err)}`);
+        formEnviarComentario.reset();
+      });
+  }
+
+}
+function validarInformacionFormulario(){
+const nombreApellido = document.getElementById('nombreApellidoContacto').value,
+email = document.getElementById('emailContacto').value, 
+numero = document.getElementById('numeroContacto').value, 
+asunto = document.getElementById('asuntoContacto').value, 
+mensaje = document.getElementById('mensajeContacto').value;
+return validarFormulario(nombreApellido, email, numero, asunto, mensaje); 
 }
