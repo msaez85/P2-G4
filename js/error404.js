572799c8b0158fe8bc015fc7cfbf06792ab1f756
Mario Saez <mario.saez@sovos.com>
2023-05-29 01:12:42 -0300

    actualizando pagina 404

--- js/error404.js
+++ js/error404.js
@@ -1,3 +1,14 @@
-window.addEventListener('DOMContentLoaded', function() {
+import { loginUsuario, cargarPaginaUsuario } from "../helpers/help.js";
+
+window.addEventListener('DOMContentLoaded', function () {
     var audio = new Audio('../assets/error404.mp3');
     audio.play();
+}
+)
+
+
+window.onload = function () {
+    cargarPaginaUsuario();
+    const btnLogin = document.getElementById('btn-login');
+    btnLogin.onclick = function () { loginUsuario() };
+}
